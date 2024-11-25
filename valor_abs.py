8767c99c1f5cb78d804035374c0276c996143c9f
AJMR2005 <arnoldmerchan2005@gmail.com>
2024-11-25 14:50:16 -0500

    first commit

---
+++ valor_abs.py
@@ -0,0 +1,17 @@
+x = 5
+
+def valor_absoluto(x):
+    if(x>=0):
+        valor= x
+    else:
+        valor = -x
+        return valor
+x = float(input("digite un numero real:"))
+##ejemplo while
+## i= iterador biena practica dejar la i como variable reservada para iteradores
+i= 0
+while(i<= 10):
+    print("el número",i)
+    print(i)
+    i += 1
+
