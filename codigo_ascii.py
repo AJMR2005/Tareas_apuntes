c7811b21d66a6d7e1df735219c7c84eaf4445900
Arnold Merchan <arnoldmerchan2005@gmail.com>
2024-11-26 21:26:31 -0500

    verdion2

--- codigo_ascii.py
+++ codigo_ascii.py
@@ -1,4 +1,4 @@
-#alrgotimo codigp ascii
+"""#alrgotimo codigp ascii
 ##se pide un numero entero dentro den numero accii
 print("ingrese un numero entero entre 1 y 255")
 num = int(input("digite un numero entero:"))
@@ -13,4 +13,9 @@ if (num >=65 and num<=90):
 elif(num<1 and num>255):
     print("el entero esta fuera de rango")
 
+##funcion descuento5%"""
+def f(x):
+    return x*x
 
+numcua =(input('ingrese el numero que desea al cuadrado'))
+print(f(5))
