e0756be70ec37cca9eaf079ee5ad7e8147e6d128
Arnold Merchan <arnoldmerchan2005@gmail.com>
2025-01-13 16:28:34 -0500

    version8

---
+++ programa.py
@@ -0,0 +1,18 @@
+'''n1=int(input('ingrese un numer'))
+n2=int(input('infrese otro num'))
+for i in range (n1,n2+1):
+    x=i*i
+    print(f'{i}al cuadrado = a {x}')
+    print(x)'''
+numero=int(input('ingrese un numero entero'))
+multi=1
+for i in range(1,numero):
+    
+    multi= multi* (i +1)
+
+    print(f'{multi}')
+
+
+
+
+
